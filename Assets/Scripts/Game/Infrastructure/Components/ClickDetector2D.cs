e34f97b3dcf07325aeadc213cbad13a85abd81dd
CrazyDev007 <gajendramr2@gmail.com>
2025-08-21 10:11:33 +0530

    Card Click on Mobile Device

--- Assets/Scripts/Game/Infrastructure/Components/ClickDetector2D.cs
+++ Assets/Scripts/Game/Infrastructure/Components/ClickDetector2D.cs
@@ -6,29 +6,22 @@ namespace Game.Infrastructure.Components
     public class ClickDetector2D : MonoBehaviour
     {
         private Camera _mainCamera;
-        private InputAction _clickAction;
 
-        private void Awake()
+        private void Awake() => _mainCamera = Camera.main;
+
+        private void Update()
         {
-            _mainCamera = Camera.main;
-            _clickAction = new InputAction(binding: "<Mouse>/leftButton");
-            _clickAction.performed += delegate
+            if (Pointer.current != null && Pointer.current.press.wasPressedThisFrame)
             {
-                var mousePosition = Mouse.current.position.ReadValue();
-                var ray = _mainCamera!.ScreenPointToRay(mousePosition);
-                var hit = Physics2D.GetRayIntersection(ray);
+                var mousePosition = Pointer.current.position.ReadValue();
+                var ray = _mainCamera.ScreenPointToRay(mousePosition);
+                var hit = Physics2D.Raycast(ray.origin, ray.direction); //.GetRayIntersection(ray);
 
                 if (hit.collider != null && hit.collider.gameObject == gameObject)
                 {
                     SendMessage("OnMouseClicked2D");
                 }
-            };
-            _clickAction.Enable();
-        }
-
-        private void OnDestroy()
-        {
-            _clickAction.Disable();
+            }
         }
     }
 }
